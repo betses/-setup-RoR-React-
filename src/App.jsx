9cf47bfc9cdc199b303ae8794abb10ae043eed85
Betsegaw Sebsibe <betsesebe@gmail.com>
2023-01-18 13:43:39 +0300

    Add linter

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [success, setSuccess] = useState('');
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const getMessage = await fetch('http://localhost:3000/message');
+      const res = await getMessage.json();
+      setSuccess(res);
+    };
+    fetchData();
+
+  }, []);
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h2>setup RoR + React project as two apps</h2>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <h3 className="read-the-docs">
+          " {success.message} " This message is displayed from backend API
+        </h3>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
